7bf98642ff65939103953efc9477703d7e488814
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:47:17 +0200

    chore: bump template

--- src/toprompt/__init__.py
+++ src/toprompt/__init__.py
@@ -1,8 +1,17 @@
+"""ToPrompt: Convert python objects to LLM-friendly descriptions"""
+
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("toprompt")
+__title__ = "ToPrompt"
+__description__ = "Convert python objects to LLM-friendly descriptions"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2025 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/toprompt"
 
 from toprompt.to_prompt import (
     Template,
